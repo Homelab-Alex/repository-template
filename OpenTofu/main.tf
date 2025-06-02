57c4cc166eda3d320b251fc70d3311d09a12b886
alexanderfreundlieb <imp.paradonix@gmail.com>
2025-06-02 16:50:33 +0200

    [feat] update branch protection

--- OpenTofu/main.tf
+++ OpenTofu/main.tf
@@ -1,5 +1,6 @@
 # Protect the main branch of the current repository
 resource "github_branch_protection_v3" "branch_protection" {
-  repository = var.current_repository
-  branch     = "main"
+  repository     = var.current_repository
+  branch         = "main"
+  enforce_admins = true
 }
